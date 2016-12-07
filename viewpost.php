375b2b7494d0433c925fb1df2277c22cabfbf08e
rawan alfaraj <rawanabdulmuhs.alfaraj@wmich.edu>
2016-12-06 22:30:18 -0500

    - added footer and info to all pages(about,stream, and viewpost)
    - live stream

--- viewpost.php
+++ viewpost.php
@@ -13,14 +13,32 @@ if($row['postID'] == ''){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <link rel="stylesheet" href="icons-css3-transitions-source/css/demo.css">
+
 <head>
     <meta charset="utf-8">
     <title>Blog - <?php echo $row['postTitle'];?></title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
-
+<div class="header">
+  <div class="content">
+    <div class="logo"> <img src="images/zoogifsm.gif" alt="Home" > </div>
+    <?php include("icons-css3-transitions-source/icons.html"); ?>
+    <div class="nav">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a class="active" href="#">About</a></li>
+        <li><a href="park.php">Park layout</a></li>
+        <li><a href="stream.php">Live Stream</a></li>
+        <li><a href="waiver.html">Waiver</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+    
 	<div id="wrapper">
 
 		<h1>Blog</h1>
@@ -35,8 +53,10 @@ if($row['postID'] == ''){
 				echo '<p>'.$row['postCont'].'</p>';				
 			echo '</div>';
 		?>
-
+        
 	</div>
-
+    <?php include("info.html"); ?>
+    <?php include("footer.html"); ?>
+    
 </body>
 </html>
